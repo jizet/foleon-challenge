b88c31fae0107ff2d6dc0da49b3b831cc1e7742e
José Ignacio Zuleta <joseigna.zuleta@gmail.com>
2019-05-15 16:14:54 +0200

    Setup API

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,15 +1,18 @@
 import apisauce from 'apisauce'
+import { environment } from './environment'
 
-const create = (baseURL = '') => {
+const create = (baseURL = 'https://www.flickr.com/services/api/render/?') => {
   const api = apisauce.create({
     baseURL,
     headers: {
-      'X-Auth-Token': process.env.SECRET_KEY
     },
     timeout: 10000
   })
 
+  const getPhotos = (tag) => api.get(`method=${environment.methods.search}&api_key=${process.env.KEY}&tags=${tag}${environment.constants.format}`)
+
   return {
+    getPhotos
   }
 }
 
---
+++ src/services/environment.js
@@ -0,0 +1,8 @@
+export const environment = {
+  constants: {
+    format: '&format=json&nojsoncallback=1'
+  },
+  methods: {
+    search: 'flickr.photos.search'
+  }
+}
